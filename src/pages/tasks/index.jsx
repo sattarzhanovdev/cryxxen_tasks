f604fd90f177b6abbed1e44d2ef1fe734892c220
sattarzhanovdev <sattarzhanovdev@gmail.com>
2024-02-28 23:05:25 +0600

    push

--- src/pages/tasks/index.jsx
+++ src/pages/tasks/index.jsx
@@ -34,11 +34,12 @@ const Tasks = () => {
           <th>Срок</th>
           <th>Оценка</th>
           <th>Постановщик</th>
-          <th>Разработчик</th>
+          <th>{name !== 'CRYXXEN' ? 'Разработчик' : 'Исполнитель'}</th>
           <th>Вид задачи</th>
           <th>Статус</th>
         </tr>
         {
+          name !== 'CRYXXEN' ?
           tasks?.map((item, i) => (
             <tr key={i}>
               <td>
@@ -58,6 +59,66 @@ const Tasks = () => {
               </td>
               <td>
                 {item.customFields?.find(item => item.name === 'Разработчик')?.value?.name }  
+              </td>
+              <td 
+                className={
+                  item.customFields?.find(item => item.name === 'Вид задачи')?.value?.name === 'Frontend' ? 
+                  c.frontend : 
+                  item.customFields?.find(item => item.name === 'Вид задачи')?.value?.name === 'Backend' ? 
+                  c.backend :
+                  item.customFields?.find(item => item.name === 'Вид задачи')?.value?.name === 'Design' ?
+                  c.design : 
+                  item.customFields?.find(item => item.name === 'Вид задачи')?.value?.name === 'Analytics' ? 
+                  c.analytics :
+                  item.customFields?.find(item => item.name === 'Вид задачи')?.value?.name === 'Testing' ? 
+                  c.testing :
+                  ''
+                }
+              >
+                {item.customFields?.find(item => item.name === 'Вид задачи')?.value?.name }  
+              </td>
+              <td
+                // className={
+                //   item.customFields?.find(item => item.name === 'State')?.value?.name === 'На тестирование' ? 
+                //   c.test : 
+                //   item.customFields?.find(item => item.name === 'State')?.value?.name === 'Приемка' ? 
+                //   c.show :
+                //   item.customFields?.find(item => item.name === 'State')?.value?.name === 'В работе' ?
+                //   c.design : 
+                //   item.customFields?.find(item => item.name === 'State')?.value?.name === 'К работе' ? 
+                //   c.analytics :
+                //   item.customFields?.find(item => item.name === 'State')?.value?.name === 'Закрыто' ? 
+                //   c.testing :
+                //   item.customFields?.find(item => item.name === 'State')?.value?.name === 'Новая' ? 
+                //   c.testing :
+                //   item.customFields?.find(item => item.name === 'State')?.value?.name === 'Релиз' ? 
+                //   c.release :
+                //   ''
+                // }
+              >
+                {item.customFields?.find(item => item.name === 'State')?.value?.name }  
+              </td>
+            </tr>
+          )) :
+          tasks?.map((item, i) => (
+            <tr key={i}>
+              <td>
+                {i+1}
+              </td>
+              <td>
+                {item.summary}
+              </td>
+              <td>
+                {convertToDate(item.customFields?.find(item => item.name === 'Due Date')?.value)}  
+              </td>
+              <td>
+                {item.customFields?.find(item => item.name === 'Оценка')?.value?.minutes }  
+              </td>
+              <td>
+                {item.customFields?.find(item => item.name === 'Постановщик')?.value?.name }  
+              </td>
+              <td>
+                {item.customFields?.find(item => item.name === 'Assignee')?.value?.name }  
               </td>
               <td 
                 className={
